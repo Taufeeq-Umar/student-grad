36dd7f44df2793f67e4925ed7ca72f105a8ec865
Taufeeq-Umar <153849926+Taufeeq-Umar@users.noreply.github.com>
2024-03-09 11:55:17 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+// logical operators
+var stdName = "Taufeeq";
+var isSlot = "Tuesday";
+var isAllowed = stdName === "Taufeeq" && isSlot === "Tuesday";
+console.log(isAllowed);
+var eMail = "taufeeq.com";
+var pasword = "123abc";
+var isAllow = true && true;
+console.log(isAllow);
+// or operators
+var Fvalue = "a";
+var Mvalue = "b";
+var iA = Fvalue === "a" || Mvalue === "b";
+console.log(iA);
+// not operators
+console.log(!(5 < 4));
+// conditional statement
+if (9 < 8) {
+    console.log("if bolack");
+}
+else {
+    console.log("else bolack");
+}
+var isPass = true;
+if (isPass) {
+    console.log("Bike");
+}
+else {
+    console.log("Abe se Mar");
+}
+var vehicle = "Car";
+if (vehicle === "Car") {
+    console.log("car ki parking");
+}
+else if (vehicle === "bike") {
+    console.log("bike ki parking");
+}
+else if (vehicle === "truck") {
+    console.log("truck ki parking");
+}
+else {
+    console.log("wapas jao");
+}
+//Task
+var grade = 90;
+if (grade === 90) {
+    console.log("A");
+}
+else if (grade === 80) {
+    console.log("B");
+}
+else if (grade === 70) {
+    console.log("C");
+}
+else if (grade === 60) {
+    console.log("D");
+}
+else if (grade === 50) {
+    console.log("E");
+}
+else
+    (grade >= 40);
+{
+    console.log("Fail");
+}
---
+++ main.ts
@@ -0,0 +1,88 @@
+// logical operators
+
+let stdName = "Taufeeq";
+let isSlot = "Tuesday";
+let isAllowed = stdName === "Taufeeq" && isSlot === "Tuesday";
+console.log(isAllowed);
+
+
+let eMail = "taufeeq.com";
+let pasword = "123abc";
+let isAllow = true && true;
+console.log(isAllow);
+
+
+// or operators
+let Fvalue = "a";
+let Mvalue = "b";
+let iA = Fvalue === "a" || Mvalue === "b";
+console.log(iA)
+
+
+// not operators
+console.log(! (5 < 4));
+
+
+// conditional statement
+if (9 < 8) {
+  console.log("if bolack");
+}
+else {
+  console.log("else bolack");
+}
+
+let isPass = true
+
+if (isPass) {
+  console.log("Bike");
+}
+else {
+  console.log("Abe se Mar");
+}
+
+
+let vehicle = "Car";
+
+if (vehicle === "Car") {
+  console.log("car ki parking");
+}
+else if (vehicle === "bike") {
+  console.log("bike ki parking");
+}
+else if (vehicle === "truck") {
+console.log("truck ki parking");
+}
+else {
+  console.log("wapas jao");
+}
+
+
+//Task
+
+let grade = 90 ;
+
+if (grade === 90) {
+  console.log("A");
+}
+else if (grade === 80) {
+  console.log("B");
+}
+else if (grade === 70){
+  console.log("C");
+}
+else if (grade === 60) {
+  console.log("D");
+}
+else if(grade === 50){
+  console.log("E");
+}
+else  (grade >= 40){
+  console.log("F");
+}
+
+
+
+
+
+
+
